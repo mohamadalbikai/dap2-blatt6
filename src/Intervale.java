b675ebcc9b92532c271ab66a46be5dcda58225d6
mohamadalbikai <mohamadbokai19@hotmail.com>
2018-05-16 11:56:55 -0700

    ich habe ein paar methoden hinzugefuegt

--- src/Intervale.java
+++ src/Intervale.java
@@ -1,6 +1,32 @@
 
 public class Intervale {
 
+	private int start;
+	private int end;
+	public Intervale(int a,int b)
+	{
+		try {
+			if(a<b)
+			{
+				start=a;
+				end=b;
+			}
+			else 
+				throw new IllegalArgumentException();
+		}catch(Exception e)
+		{
+			System.out.println("der Interval war falsch : die war kleiner als Anfang");
+			throw new IllegalArgumentException();
+		}
+	}
+	public int getStart() 
+	{
+		return start;
+	}
+	 public int getEnd() 
+	 {
+		 return end;
+	 }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
     System.out.println("ich bin hier ");
